a56ef5e3d8ca5afc00dc451b9d5db9e85c4dfb79
s_nega <snezhana.golovko@gmail.com>
2024-10-14 17:24:22 +0500

    lab5 added keycloak and dependencies

---
+++ micro01/src/main/java/kz/bitlab/middle02/micro01/micro01/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package kz.bitlab.middle02.micro01.micro01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth.anyRequest().authenticated()
+                )
+                .sessionManagement(
+                        session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).oauth2ResourceServer(
+                        o -> o
+                                .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+
+}
